9883a38ed0a11552d8211e1a183b86970c48275e
yangyyou <wuyoyang@gmail.com>
2023-11-19 17:27:51 +0800

    配置初始化，配置eslint prettier

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Quasar App (-region-shop-admin)
+# Quasar App (quasar-project)
 
 A Quasar Project
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,8 @@ import {
 
 import routes from './routes';
 
+const a = 'cdf';
+
 /*
  * If not building with SSR mode, you can
  * directly export the Router instantiation;
@@ -20,7 +22,9 @@ import routes from './routes';
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
